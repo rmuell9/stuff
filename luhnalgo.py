f5aa4fce6b681d3f31745b5774289f3a50cd4cf7
Matthew Mueller <rmatthewmueller@gmail.com>
2025-03-17 01:35:18 +0000

    Add files via upload

--- luhnalgo.py
+++ luhnalgo.py
@@ -3,17 +3,25 @@
 def verify(digits):
     total = 0
     for i, d in enumerate(reversed(digits[:-1])): #dropping the check digit from the number
-        if i % 2 == 0:
-            multiplier = 2
-        else:
-            multiplier = 1
-        x = int(d) * multiplier #creating payload digits by multiplying every second digit by two
+        x = int(d) * (2 - i % 2) #creating payload digits by multiplying every second digit by two
         xx = x // 10 + x % 10 #Adds the digits in a two digit num together, doesnt work with higher digit numbers 
         total = total + xx 
-    check = 10 - (total % 10) #calculating check digit with (10 - (total mod10))mod10
-    if int(digits[-1:]) == check:
+    check = 10 - (total % 10) % 10 #calculating check digit with (10 - (total mod10))mod10)
+    if check == int(digits[-1]):
         print('valid')
-    else:
+    else: 
         print('invalid')
 
 verify('17893729974')
+
+def efficientVerify(digits):
+    total = 0
+    for i, d in enumerate(reversed(digits)):
+        x = int(d) * (1 + i % 2)
+        total += x // 10 + x
+    return total % 10 == 0
+
+if __name__ == '__main__':
+    assert efficientVerify('17893729974')
+    assert not efficientVerify('17893729975')
+    print('valid too')
